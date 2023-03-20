67fc62d7aa3a6707d901dfb77a5be152957bcc41
charles otonda <charlesotonda@gmail.com>
2023-03-20 15:24:32 -0700

    combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,15 +16,18 @@ int main(void)
 	{
 		for (j = 1 ; j < 10 ; j++)
 		{
-			for (k = 2 ; k > 10 ; k++)
+			for (k = 2 ; k < 10 ; k++)
 			{
 				if (i < j && j < k)
 				{
 					putchar (i + '0');
 					putchar (j + '0');
 					putchar (k + '0');
+					if (i + j + k != 24)
+					{
 					putchar (',');
 					putchar (' ');
+					}
 				}
 			}
 		}
