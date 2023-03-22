2a50847d6bdebc12a6e68b85f9971cefc24b401b
charles otonda <charlesotonda@gmail.com>
2023-03-21 19:52:13 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - sum two intergers and return result
+ * @a: integer 1
+ * @b: integer 2
+ * Return: sum of two intergers
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
