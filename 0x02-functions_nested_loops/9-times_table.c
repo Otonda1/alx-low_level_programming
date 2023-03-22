a40ea7a481dfa76cb94b2042dee0dc232d3e4418
charles otonda <charlesotonda@gmail.com>
2023-03-21 19:40:38 -0700

    late

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the multiplication table
+ * of 9
+ *
+ */
+
+void times_table(void)
+{
+	int r, c, p;
+
+	for (r = 0 ; r <= 9 ; r++)
+	{
+		_putchar('0');
+		for (c = 1 ; c <= 9 ; c++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			p = (r * c);
+			if (p <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((p / 10) + '0');
+			}
+			_putchar((p % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
