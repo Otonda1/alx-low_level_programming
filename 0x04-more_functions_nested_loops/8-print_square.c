ba5c7c92dcbe310cec4ac20a37f1c6ceee3ac013
charles otonda <charlesotonda@gmail.com>
2023-03-23 13:36:31 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - Draws square on terminal
+ * @size: Argument (Integer size of square)
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size; i++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
+
