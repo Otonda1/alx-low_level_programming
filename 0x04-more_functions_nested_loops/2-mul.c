75800b6e7814c9defc5d15e04c4335b260796610
charles otonda <charlesotonda@gmail.com>
2023-03-23 10:55:37 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two digit
+ * @a: integer 1
+ * @b: integer 2
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
+
