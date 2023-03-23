95247acd334ccc2b9e8f146696b22e5d5b5c0fc3
charles otonda <charlesotonda@gmail.com>
2023-03-23 09:18:36 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase
+ * Return: 1 if uppercase 0 otherwise
+ * @c: parameter value
+ */
+
+int _isupper(int c)
+{
+	int i;
+
+	if (c >= 65 && c <= 90)
+		i = 1;
+	else
+		i = 0;
+	return (1);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+int _isupper(int c);
+
+#endif
