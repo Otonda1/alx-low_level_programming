a7e8c41bcb7e77f26a33b3391854a7b2ce44bd78
charles otonda <charlesotonda@gmail.com>
2023-03-27 11:06:28 -0700

    swaps

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,17 @@
 #include "main.h"
 
+/**
+ * swap_int - swaps the values of two variables
+ *
+ * @a: variable 1
+ * @b: variable 2
+ */
+
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
 }
