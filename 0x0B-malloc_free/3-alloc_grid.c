c4b6eec9669182587f704cd175f5d44ef66436fd
charles otonda <charlesotonda@gmail.com>
2023-04-11 16:00:14 -0700

    returns apointer to a dimension of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * alloc_grid - points to a 2D array of integers
+ *
+ * @width: matrix width
+ * @height: matrix height
+ * Return: 0 success, 1 otherwise
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, **c;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	c = (int **)malloc(sizeof(int *) * height);
+	if (c == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		c[i] = (int *)malloc(sizeof(int) * width);
+		if (c[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(c[i]);
+				i--;
+			}
+			free(c);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		c[i][j] = 0;
+	}
+	return (c);
+}
