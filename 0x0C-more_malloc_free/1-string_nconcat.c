9e6eb41d51242d4184cac50e0509bba67f0d65cd
charles otonda <charlesotonda@gmail.com>
2023-04-14 09:30:32 -0700

    concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,31 +1,38 @@
 #include "main.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * string_nconcat - concatenates two strings
  * @s1: string 1
  * @s2: string 2
  * @n: length of s2 to append to s1
- * Return: pointer to concatenated string
+ * Return: pointer to concatenated string, or NULL on failure
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j;
+	unsigned int i, j, s1_len, s2_len;
 	char *str;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 == "";
-	for (i = 0; s1[i] != '\0'; i++)
+		s2 = "";
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+	if (n == 0)
 	{
+		str = malloc(sizeof(char) * (s1_len + 1));
+		if (str == NULL)
+			return (NULL);
+		strcpy(str, s1);
+		return (str);
 	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-	if (n >= j)
-		n = j;
-	str = malloc(sizeof(char) * (i + j + 1));
+	if (n > s2_len)
+		n = s2_len;
+	str = malloc(sizeof(char) * (s1_len + n + 1));
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
