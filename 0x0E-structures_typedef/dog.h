f76d1d31449f746f9949340e7bf4b59438316e7c
charles otonda <charlesotonda@gmail.com>
2023-04-17 12:06:47 -0700

    free dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdio.h>
 
 /**
@@ -16,3 +19,9 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* main.h */
