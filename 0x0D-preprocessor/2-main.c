b2f21fe37c0756203e892628318caf1490a9f8d4
charles otonda <charlesotonda@gmail.com>
2023-04-19 06:18:54 -0700

    prints the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of file its compiled
+ *		from
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s", _FILE_);
+	return (0);
+}
