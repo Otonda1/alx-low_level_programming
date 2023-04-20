8dd73e71f93da5d3e864e64af5bca62497bc8c5f
charles otonda <charlesotonda@gmail.com>
2023-04-20 13:44:14 -0700

    prints strings followed by a new line

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints string, followed
+ *		by newline
+ * @separator: separator string
+ * @n: number of strings
+ *
+ * Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list pr;
+	unsigned int i;
+	char *ch;
+
+	va_start(pr, n);
+	for (i = 0; i < n; i++)
+	{
+		ch = va_arg(pr, char *);
+		if (ch == NULL)
+			printf("(nil)");
+		else
+		{
+		printf("%s", ch);
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
+		}
+	}
+	va_end(pr);
+	printf("\n");
+}
