56ffd2472a0c74fa44eba4ec62cde7b3ddea3749
charles otonda <charlesotonda@gmail.com>
2023-04-21 13:24:01 -0700

    prints anything

--- 0x10-variadic_functions/mchezo.c
+++ 0x10-variadic_functions/mchezo.c
@@ -6,7 +6,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
         va_list arg;
         unsigned int i;
 
-        if (separator == NULL)
+        if (separator == NULL && i < (n - 1))
                 return;
         va_start(arg, n);
         for (i = 0 ; i < n ; i++)
