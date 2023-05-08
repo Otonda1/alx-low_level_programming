3d800c711796d48aa4b0f8c37bbc2cf6c5e6c203
charles otonda <charlesotonda@gmail.com>
2023-05-08 15:25:22 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,34 +9,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	char *buffer;
-	int fd, rd, bytesize;
-	FILE *fp;
+	int op, wrt, strlen = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
+
+	if (text_content != NULL)
 	{
-		fp = fopen(filename, "w");
-		if (fp == NULL)
-			return (-1);
-		fclose(fp);
+		while (text_content[strlen] != '\0')
+			strlen++;
 	}
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wrt = write(op, text_content, strlen);
 
-	buffer = malloc(sizeof(text_content));
-	if (buffer == NULL)
+	if (op == -1 || wrt == -1)
 		return (-1);
-
-	fd = open(filename, O_CREAT, 600);
-	rd = read(fd, buffer, sizeof(text_content));
-	bytesize = write(fd, buffer, rd);
-
-	if (fd == -1 || rd == -1 || bytesize == -1)
-	{
-		free(buffer);
-		return (-1);
-	}
-	free(buffer);
-	close(fd);
+	close(op);
 	return (1);
 }
