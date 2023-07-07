04890022fccbe3eec35da797166bd9b9b2b813c8
charles otonda <charlesotonda@gmail.com>
2023-07-07 04:57:52 -0700

    a function that gives the index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,45 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds element to hash table
- * @key: the element's key
- * @value:said element's value
- * @ht: pointer to the hash table
- * Return: 1 on success, 0 otherwise
+ * key_index - finds index of key in hash table
+ *
+ * @key: Immutable key for each element
+ * @size: size of hash table
+ * Return: array index of the key
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long index, size;
-	hash_node_t *new_node, *temp;
-
-	if (ht == NULL || key == NULL || value == NULL)
-		return (0);
-
-	size = ht->size;
-	index = key_index((const unsigned char *)key, size);
-	temp = ht->array[index];
-
-	if (temp != NULL)
-	{
-		if (strcmp(temp->key, key) == 0)
-		{
-			free(temp->value);
-			temp->value = strdup(value);
-			if (temp->value == NULL)
-				return (0);
-			return (1);
-		}
-		temp = temp->next;
-	}
-
-	new_node = malloc(sizeof(hash_node_t));
-
-	if (new_node == NULL)
-		return (0);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-	return (1);
+	return (hash_djb2(key) % size);
 }
